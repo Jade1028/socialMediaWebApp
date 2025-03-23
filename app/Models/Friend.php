276d5dc3c1c0235b1ba51f277bf3dfa813b58416
J1nnHAlt <jinhaowngaltacc@gmail.com>
2025-03-23 20:32:24 +0800

    useless relationship removed

--- app/Models/Friend.php
+++ app/Models/Friend.php
@@ -9,15 +9,4 @@ class Friend extends Model
 {
     use HasFactory;
 
-    /*
-        The friend tables use the primary key of the user table
-        Therefore, the friend belongs to a user table 
-    */
-    public function user1(){
-        return $this->belongsTo(User::class, 'user_id1');
-    }
-    
-    public function user2(){
-        return $this->belongsTo(User::class, 'user_id2');
-    }
 }
