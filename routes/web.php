e62167e762506f95fafdb46db1934e4aae546c7c
Jade1028 <weijie2828@gmail.com>
2025-03-31 11:58:01 +0800

    refactored to resource routes

--- routes/web.php
+++ routes/web.php
@@ -58,13 +58,7 @@ Route::middleware('auth')->controller(UserController::class)->group(function(){
 
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-    Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
-    Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
-    Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
-    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+    Route::resource('posts', PostController::class);
     Route::post('/posts/{postId}/toggle-like', [PostController::class, 'toggleLike'])->name('posts.toggleLike');
     Route::post('/posts/{postId}/comment', [PostController::class, 'addComment'])->name('posts.comment');
 });
