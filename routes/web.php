34ddd759fd911b73fdc2b2ede556a285ac26fe9d
J1nnHAlt <jinhaowngaltacc@gmail.com>
2025-04-12 16:04:37 +0800

    formatted

--- routes/web.php
+++ routes/web.php
@@ -29,12 +29,12 @@ Route::get('/', function () {
 
 Auth::routes(); // This is the default authentication routes: /login, /register, /logout
 
-Route::controller(LoginController::class)->group(function(){
+Route::controller(LoginController::class)->group(function () {
     Route::get('/login/admin', 'showAdminLoginForm');
     Route::post('/login/admin', 'adminLogin')->middleware('throttle:3,10'); // Limit to 3 attempts every 10 minutes
 });
 
-Route::controller(RegisterController::class)->group(function(){
+Route::controller(RegisterController::class)->group(function () {
     Route::get('/register/admin', 'showAdminRegisterForm');
     Route::post('/register/admin', 'createAdmin');
 });
@@ -47,15 +47,13 @@ Route::controller(AdminController::class)->middleware('auth:admin')->group(funct
 
 Route::get('logout', [LoginController::class, 'logout']);
 
-Route::controller(HomeController::class)->group(function(){
-    Route::get('/about','about')->name('about');
-    Route::get('/contact','contact')->name('contact');
-    Route::get('/profile','profile')->name('profile');
+Route::controller(HomeController::class)->group(function () {
+    Route::get('/about', 'about')->name('about');
+    Route::get('/contact', 'contact')->name('contact');
+    Route::get('/profile', 'profile')->name('profile');
 });
 
-
-
-Route::controller(MessageController::class)->group(function(){
+Route::controller(MessageController::class)->group(function () {
     Route::get('/message/{id}', 'index')->name('messages.index');
     Route::post('/message/{id}', 'store')->name('message.send');
     Route::get('/message/edit/{id}', 'edit')->name('message.edit');
@@ -63,26 +61,24 @@ Route::controller(MessageController::class)->group(function(){
     Route::delete('/message/delete/{id}', 'destroy')->name('message.delete');
 });
 
-Route::middleware('auth')->controller(FriendController::class)->group(function(){
-    Route::get('/friends','index')->name('friends');
-    Route::get('/friends/add/{id}','store')->name('friends.store');
-    Route::get('/friends/accept/{id}','accept')->name('friends.accept');
-    Route::get('/friends/reject/{id}','reject')->name('friends.reject');
-    Route::get('/friends/remove/{id}','destroy')->name('friends.destroy');
+Route::middleware('auth')->controller(FriendController::class)->group(function () {
+    Route::get('/friends', 'index')->name('friends');
+    Route::get('/friends/add/{id}', 'store')->name('friends.store');
+    Route::get('/friends/accept/{id}', 'accept')->name('friends.accept');
+    Route::get('/friends/reject/{id}', 'reject')->name('friends.reject');
+    Route::get('/friends/remove/{id}', 'destroy')->name('friends.destroy');
 });
 
-Route::middleware('auth')->controller(UserController::class)->group(function(){
-    Route::get('/users','userView')->name('users.view');
-    Route::get('/users/search','search')->name('users.search');
+Route::middleware('auth')->controller(UserController::class)->group(function () {
+    Route::get('/users', 'userView')->name('users.view');
+    Route::get('/users/search', 'search')->name('users.search');
 });
 
 
 Route::middleware('auth')->group(function () {
-    Route::resource('posts', PostController::class); 
-    Route::post('/posts/{postId}/toggle-like', [PostController::class,'toggleLike'])->name('posts.toggleLike');
-    Route::post('/posts/{postId}/comment', [PostController::class,'addComment'])->name('posts.comment');
-    Route::get('/home', [PostController::class,'index'])->name('home');
-    Route::resource('comments', CommentController::class)->except(['index', 'show']); 
+    Route::resource('posts', PostController::class);
+    Route::post('/posts/{postId}/toggle-like', [PostController::class, 'toggleLike'])->name('posts.toggleLike');
+    Route::post('/posts/{postId}/comment', [PostController::class, 'addComment'])->name('posts.comment');
+    Route::get('/home', [PostController::class, 'index'])->name('home');
+    Route::resource('comments', CommentController::class)->except(['index', 'show']);
 });
-
-
